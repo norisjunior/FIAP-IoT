b7a75a37058eeb780b3791498da7b56e2c0492da
Ferraz Junior, N. <norisjunior@gmail.com>
2025-11-28 16:20:27 -0300

    ajustes iniciais app21 para novo potenciometro

--- app21-AIoT-RF-Occupancy_mc2gen/src/ESP32Sensors.hpp
+++ app21-AIoT-RF-Occupancy_mc2gen/src/ESP32Sensors.hpp
@@ -3,11 +3,13 @@
 #include "ESP32SensorsLED.hpp"       // LED indicador
 #include "ESP32SensorsLDR.hpp"       // LDR (luminosidade)
 #include "ESP32SensorsCO2.hpp"       // CO2 simulado pelo potenciômetro
+#include "ESP32SensorsHumidityRatio.hpp"       // HR simulado pelo potenciômetro
 namespace ESP32Sensors {
-  void beginAll(uint8_t dhtPin, uint8_t dhtModel, uint8_t ledPin, uint8_t ldrPin, uint8_t CO2Pin) {
+  void beginAll(uint8_t dhtPin, uint8_t dhtModel, uint8_t ledPin, uint8_t ldrPin, uint8_t CO2Pin, uint8_t HRPin) {
     Ambiente::inicializar(dhtPin, dhtModel);
     LED::inicializar(ledPin);
     LDR::inicializar(ldrPin);
     CO2::inicializar(CO2Pin);
+    HR::inicializar(HRPin);
   }
 }
