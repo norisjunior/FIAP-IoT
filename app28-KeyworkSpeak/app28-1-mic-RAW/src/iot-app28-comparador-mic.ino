b010d893b8e30d2ad01e6f4c07b29c275b143822
Ferraz Junior, N. <norisjunior@gmail.com>
2026-04-20 12:19:51 -0300

    .

---
+++ app28-KeyworkSpeak/app28-1-mic-RAW/src/iot-app28-comparador-mic.ino
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+#define MIC_PIN_KY  34
+#define MIC_PIN_MAX 35
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int mic_ky = analogRead(MIC_PIN_KY);
+  int mic_max = analogRead(MIC_PIN_MAX);
+
+  Serial.printf(">som_ky: %d\n",mic_ky); Serial.println("");
+  Serial.printf(">som_max: %d\n",mic_max); Serial.println("");
+
+  delay(2); // amostragem mais rápida
+}
