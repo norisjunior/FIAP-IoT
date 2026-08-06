f54a9e7e8b68fe7f282713e42b2190efc4265c9b
Noris Junior <norisjunior@gmail.com>
2026-08-06 17:07:27 -0300

    .

--- app01-dht/src/iot-aula01-app01.ino
+++ app01-dht/src/iot-aula01-app01.ino
@@ -9,14 +9,8 @@
 #define DHTMODE DHT22
 DHT dht(DHTPIN, DHTMODE);
 
-void piscaLED() {
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(LEDPIN_VERDE, HIGH);
-    delay(1000);
-    digitalWrite(LEDPIN_VERDE, LOW);
-    delay(1000);
-  }
-}
+uint32_t INTERVALO_COLETA = 2100;
+uint32_t tempoAgora = 0;
 
 void setup() {
   //Inicializar sensores
@@ -28,38 +22,35 @@ void setup() {
 }
 
 void loop() {
-  bool acenderLed = false;
+  if (millis() >= tempoAgora + INTERVALO_COLETA) {
+    tempoAgora = millis();
 
-  float temp = dht.readTemperature();
-  float umid = dht.readHumidity();
+    bool acenderLed = false;
 
-  if (isnan(temp) || isnan(umid)) {
-    Serial.println("Falha ao ler do sensor DHT!");
-    delay(2000);
-    return;
+    float temp = dht.readTemperature();
+    float umid = dht.readHumidity();
+
+    if (isnan(temp) || isnan(umid)) {
+      Serial.println("Falha ao ler do sensor DHT!");
+      delay(2000);
+      return;
+    }
+
+    float heatIndex = dht.computeHeatIndex(temp, umid, false);
+
+    Serial.printf("\nT: %.2f | U: %.2f | IC: %.2f\r\n", temp, umid, heatIndex);
+
+    if ( (heatIndex > 25) ) {
+      Serial.println("[WARN] Condições ambientais inadequadas");
+      acenderLed = true;
+    } else {
+      Serial.println("[OK] Condições ambientais dentro da normalidade");
+      acenderLed = false;
+    }
+    digitalWrite(LEDPIN_VERMELHO, acenderLed ? HIGH : LOW);
+    //ou
+    //digitalWrite(LEDPIN_VERMELHO, acenderLed);
   }
-
-  float heatIndex = dht.computeHeatIndex(temp, umid, false);
-
-  Serial.print("\nTemperatura: "); Serial.print(temp);
-  Serial.print(" °C, Umidade: "); Serial.print(umid);
-  Serial.print(" %, Índice de calor: "); Serial.print(heatIndex); Serial.println(" °C");
-
-  if ( (heatIndex > 25) ) {
-    Serial.println("Condições ambientais inadequadas");
-    acenderLed = true;
-  } else {
-    Serial.println("Condições ambientais dentro da normalidade");
-    acenderLed = false;
-  }
-  digitalWrite(LEDPIN_VERMELHO, acenderLed ? HIGH : LOW);
-  //ou
-  //digitalWrite(LEDPIN_VERMELHO, acenderLed);
-
-  delay(2000); // Aguarda 2 segundos antes da próxima leitura (sensor DHT)
-
-  // Pisca o LED para indicar que começará a próxima leitura
-  piscaLED();
-  delay(1000);
+  
 }
 
